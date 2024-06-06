001ec464fc9866c493f06b708737ac43927b5d96
Maksim Kamenev <maksim-kamenev96@mail.ru>
2024-06-06 18:24:01 +0300

    app_name

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
+from catalog.apps import CatalogConfig
 from catalog.views import home, contacts
 
-app_name = 'catalog'
+app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
